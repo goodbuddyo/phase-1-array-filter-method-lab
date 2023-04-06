5ad89f10b4166720af4c7a34e622f9b5f96618a5
Seano <sean01234@yahoo.com>
2023-04-06 03:14:12 -0400

    filter tests

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution here
+
+const driverNames = ['Tom', 'Joe', 'Pete'];
+
+function findMatching(drivers, query) {
+  return drivers.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+}
+
+console.log('findMatching');
+console.log(findMatching(driverNames, "te"));
+console.log(driverNames);
+
+// const driverNames = ['Tom', 'Joe', 'Pete'];
+
+function fuzzyMatch(drivers, query) {
+  return drivers.filter(driver => driver.startsWith(query));
+}
+
+console.log('fuzzyMatch');
+console.log(fuzzyMatch(driverNames, "Jo"));
+console.log(driverNames);
+
+const arrDriverObj = [
+  {
+    "hometown": "NYC",
+    "name": "Tom"
+  },
+  {
+    "hometown": "Pittsburgh",
+    "name": "Bobby"
+  },
+  {
+    "hometown": "Tampa Bay",
+    "name": "Bobby"
+  }
+];
+
+function matchName(drivers, query) {
+  const filterName = arrDriverObj.filter(el =>
+    el.name === query
+  );
+  return filterName
+}
+console.log('matchName');
+console.log(matchName(arrDriverObj, "Tom"))
+console.log(driverNames);
